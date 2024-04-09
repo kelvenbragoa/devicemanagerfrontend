c345c5044b6296417b72388ff7abe078290fc575
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-04-09 16:27:06 +0200

    feat: restrict user menu for admin and others roels

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 import AppMenuItem from './AppMenuItem.vue';
-
-const model = ref([
+const model = ref([]);
+const modelAdmin = ref([
     {
         label: 'Painel',
         items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/dashboard' }]
@@ -27,7 +27,7 @@ const model = ref([
         label: 'Operação',
         items: [
             { label: 'Gestao de Entregas', icon: 'pi pi-fw pi-database', to: '/deliveries' },
-            { label: 'Transações', icon: 'pi pi-fw pi-database', to: '/transactions' },
+            { label: 'Transações', icon: 'pi pi-fw pi-sort-alt', to: '/transactions' },
             { label: 'Auditoria de Registros', icon: 'pi pi-fw pi-history', to: '/audits' }
         ]
     },
@@ -39,6 +39,53 @@ const model = ref([
         ]
     }
 ]);
+
+const modelUser = ref([
+    {
+        label: 'Painel',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/dashboard' }]
+    },
+    {
+        label: 'Gestão',
+        items: [
+            { label: 'Usuarios', icon: 'pi pi-fw pi-user', to: '/users' },
+            { label: 'Empresas', icon: 'pi pi-fw pi-id-card', to: '/companies' },
+            { label: 'Trabalhadores', icon: 'pi pi-fw pi-table', to: '/employees' }
+        ]
+    },
+    {
+        label: 'Dispositivos',
+        items: [
+            { label: 'Tipos de Dispositivos', icon: 'pi pi-fw pi-bars', to: '/typedevices' },
+            { label: 'Dispositivos', icon: 'pi pi-fw pi-tablet', to: '/devices', badge: 'NEW' }
+        ]
+    },
+    {
+        label: 'Operação',
+        items: [
+            { label: 'Gestao de Entregas', icon: 'pi pi-fw pi-database', to: '/deliveries' },
+            { label: 'Transações', icon: 'pi pi-fw pi-sort-alt', to: '/transactions' },
+            // { label: 'Auditoria de Registros', icon: 'pi pi-fw pi-history', to: '/audits' }
+        ]
+    },
+    {
+        label: 'Configurações',
+        items: [
+            { label: 'Perfil', icon: 'pi pi-fw pi-user', to: '/profile' }
+            // { label: 'LogOut', icon: 'pi pi-fw pi-unlock', to: '/dashboards' }
+        ]
+    }
+]);
+const user = ref([]);
+
+onMounted(() => {
+    user.value = JSON.parse(localStorage.getItem('user'));
+    if (user.value.role_id == 1 || user.value.role_id == 2) {
+        model.value = modelAdmin.value;
+    } else {
+        model.value = modelUser.value;
+    }
+});
 </script>
 
 <template>
